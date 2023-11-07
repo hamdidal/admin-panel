b443861f7c8dc7d454e78cf9786f632d11af55fa
icht4i <hamdidal@yahoo.com>
2023-11-07 14:59:30 +0300

    initial

--- src/components/styledComponents/Input/Select/Select.styled.tsx
+++ src/components/styledComponents/Input/Select/Select.styled.tsx
@@ -1,45 +1,55 @@
-import { InputLabel, MenuItem, Select, styled } from '@mui/material'
-import { colors } from '../../../../styles/color';
+import { InputLabel, MenuItem, Select, styled } from "@mui/material";
+import { colors } from "../../../../styles/color";
 
-export const SelectContainer = styled('div')((props: { position: 'left' | 'top'; width: string | number }) => ({
+export const SelectContainer = styled("div")(
+  (props: { position: "left" | "top"; width: string | number }) => ({
     width: props.width,
-    display: 'flex',
-    flexDirection: props.position === 'top' ? 'column' : 'row',
-    alignItems: props.position === 'left' ? 'center' : 'normal',
-    gap: props.position === 'left' ? '10px' : '8px',
-}))
+    display: "flex",
+    flexDirection: props.position === "top" ? "column" : "row",
+    alignItems: props.position === "left" ? "center" : "normal",
+    gap: props.position === "left" ? "10px" : "8px",
+  })
+);
 
-export const CustomSelect = styled(Select)((props: { position: 'left' | 'top'; size: 'small' | 'medium' }) => ({
-    width: '100%',
-    minWidth: props.size === 'medium' ? '220px' : '100%',
-    height: props.size === 'medium' ? '56px' : '38px',
-    '&.Mui-focused': {
-        '& .MuiOutlinedInput-notchedOutline': {
-            border: '2px solid #FFAA00',
+export const CustomSelect = styled(Select)(
+  (props: { position: "left" | "top"; size: "small" | "medium" }) => ({
+    width: "100%",
+    minWidth: props.size === "medium" ? "220px" : "100%",
+    height: props.size === "medium" ? "56px" : "38px",
+    "&.Mui-focused": {
+      "& .MuiOutlinedInput-notchedOutline": {
+        border: "2px solid #FFAA00",
+      },
+      "& .MuiOutlinedInput-root": {
+        "&.Mui-focused fieldset": {
+          borderColor: colors.primary.primaryMain,
         },
-        '& .MuiOutlinedInput-root': {
-            '&.Mui-focused fieldset': {
-                borderColor: colors.primary.primaryMain,
-            },
-        },
+      },
     },
 
-    '&:before': {
-        borderColor: colors.primary.primaryMain,
+    "&:before": {
+      borderColor: colors.primary.primaryMain,
     },
-    '&:after': {
-        borderColor: colors.primary.primaryMain,
+    "&:after": {
+      borderColor: colors.primary.primaryMain,
     },
-}))
+  })
+);
 export const CustomInputLabel = styled(InputLabel)(() => ({
-    '&.Mui-focused': {
-        color: colors.primary.primaryMain,
-        backgroundColor: colors.lightBackground,
+  "&.Mui-focused": {
+    color: colors.primary.primaryMain,
+    backgroundColor: colors.lightBackground,
 
-        '& .MuiOutlinedInput-notchedOutline': {
-            color: colors.primary.primaryMain,
-            backgroundColor: colors.lightBackground,
-        },
+    "& .MuiOutlinedInput-notchedOutline": {
+      color: colors.primary.primaryMain,
+      backgroundColor: colors.lightBackground,
     },
-}))
-
+  },
+}));
+export const CustomMenuItem = styled(MenuItem)({
+  // TODO: It will be updated according to the design
+  // backgroundColor: '',
+  // '&&.Mui-selected': '',
+  // '&&:hover': '',
+  // '&&.Mui-focusVisible': '',
+});
